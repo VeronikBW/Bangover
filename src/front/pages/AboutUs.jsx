6230ea47148a78b46cdad4b88ff3176d4f40b19e
Verónica Rodríguez <veronikbw@gmail.com>
2026-04-04 18:10:23 +0000

    Se empieza a configurar la página about us

--- src/front/pages/AboutUs.jsx
+++ src/front/pages/AboutUs.jsx
@@ -3,8 +3,35 @@ import "../styles/pages/AboutUs.css";
 export const AboutUs = () => {
 
     return (
-        <div className="aboutus-page">
-            <h1>About Us</h1>
+        <div className="aboutus-body">
+            <div className="aboutus-card-wrapper">
+                <div className="aboutus-card">
+                    <div className="Presentacion">
+                        <h1>*» Registro de FC
+                            » Internacional
+                            » Sin temática
+                        </h1>
+                    </div>
+                    <div className="parrafo_aboutUS">
+                        <p>El término "Bangover" proviene de Hangover (resaca). Significa el malestar muscular que queda después de un encuentro sexual intenso. Bangover no solo hace referencia al dolor postsexo, sino también a un encuentro sexual intenso de seis horas en el que el tiempo se emplea únicamente para eso: sexo.
+                            Bangover RPG se propone ser un espacio narrativo dedicado a uno de los mejores placeres de la vida: el sexo. El sexo y sus infinitas formas de practicarlo. Contamos con una extensa lista de actividades que, en su gran mayoría, pueden ser adaptadas a cualquier temática. Actividades sexuales, por supuesto, y también no sexuales. Nosotros ponemos a tu disposición esa lista y esperamos que le saques el mayor provecho.
+                            Para ser parte de esta casa, te vamos a pedir que escribas para nosotros (en el apartado de FC encontrarás esta información). Pero antes… las reglas.  </p>
+                    </div>
+                    <div className="cita_aboutUS">
+                        <p>❝ Si es el elemento sucio el que da placer al acto de lujuria, entonces cuanto más sucio sea, más placentero será.
+                            - Marqués de Sade.</p>
+                    </div>
+                    <div className="rules_aboutUS">
+                        <ul>
+                            <li> <p className="rule-title">Respeto</p>
+                                <p>Se permiten dramas on-rol; off-rol no.</p>
+                                <p>Si surge un problema off-rol que los implicados no pueden resolver, pueden acudir a la administración.</p>
+                                <p>Los problemas personales quedan fuera.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
